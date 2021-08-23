0c5c911739cc3ddea092c9f48167b096a0764118
xinlinrong <549716158@qq.com>
2021-08-23 23:37:55 +0800

    创建 k8s installer
    创建 k8s installer

---
+++ installer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cp docker-ce.repo /etc/yum.repo.d/
+cp kubernetes.repo /etc/yum.repo.d/
+
+# 安装 docker
+yum install -y docker-ce docker-ce-cli containerd containerd.io
+
+# 安装 k8s
+yum install -y kubelet-1.20* kubeadm-1.20* kubectl-1.20* --disableexclude=kubernetes
+
+yum install -y kubelet kubeadm --disableexclude=kubernetes
